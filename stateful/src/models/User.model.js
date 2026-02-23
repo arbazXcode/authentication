840606a50b9fe323f88e66cab572b87bad18f27c
arbazXcode <jmi.arbaz1@gmail.com>
2026-02-24 03:26:30 +0530

    feat: built user model

--- stateful/src/models/User.model.js
+++ stateful/src/models/User.model.js
@@ -1,2 +1,36 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
 
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minLength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    }
+});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next()
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password)
+}
